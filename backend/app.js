4ab509b52d3ef07510372bfb8082c11a662ae451
yodamester <yodamester@users.noreply.github.com>
2023-10-03 06:13:45 +0200

    removed unused import

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const { ApolloServer } = require('apollo-server-express');
-const { gql } = require('apollo-server-express');
 require("dotenv").config();
 
 const app = express();
